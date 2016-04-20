f6e97142f42eb752a3ddd7cd5844b261ad14fa83
Matt Hackmann <matt@dxprog.com>
2016-04-19 21:34:56 -0700

    initial styles

--- js/image-cell.js
+++ js/image-cell.js
@@ -1,6 +1,7 @@
 function ImageCell(parent) {
   this.parent = parent;
   this.el = document.createElement('div');
+  this.el.classList.add('image-cell');
   this.image = document.createElement('img');
   this.el.appendChild(this.image);
   this._inDOM = false;
@@ -8,7 +9,12 @@ function ImageCell(parent) {
 
 ImageCell.prototype = {
   render: function update(data) {
-    this.image.src = data.src;
+    var el = this.el;
+    var image = this.image;
+    el.style.backgroundImage = 'url(' + data.src + ')';
+    image.src = data.src;
+    image.setAttribute('alt', data.title);
+    image.setAttribute('title', data.title);
     if (!this._inDOM) {
       this.parent.appendChild(this.el);
       this._inDOM = true;
