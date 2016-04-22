e8d5f80976a9173c453b1e45249585b87970918a
Matt Hackmann <matt@dxprog.com>
2016-04-21 22:33:27 -0700

    added docs to ImageCell

--- js/image-cell.js
+++ js/image-cell.js
@@ -1,3 +1,9 @@
+/**
+ * A single Image cell for use in an ImageGride
+ *
+ * @constructor
+ * @param {HTMLElement} parent The cell's parent element
+ */
 function ImageCell(parent) {
   this.parent = parent;
   this.el = document.createElement('div');
@@ -8,6 +14,12 @@ function ImageCell(parent) {
 }
 
 ImageCell.prototype = {
+  /**
+   * Updates the cell with new data and renders it
+   *
+   * @method render
+   * @param {Object} data The data to render. Ex: { src, title }
+   */
   render: function update(data) {
     var el = this.el;
     var image = this.image;
