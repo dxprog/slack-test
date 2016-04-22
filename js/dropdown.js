dbd91c94690d571741fb46edaf473491f58a3c2a
Matt Hackmann <matt@dxprog.com>
2016-04-21 22:31:50 -0700

    docs + getValue method for dropdown

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,15 +1,33 @@
+/**
+ * Wrapper for some basic functionality on a <select> element
+ *
+ * @constructor
+ * @param {HTMLSelectElement} el The select element to use.
+ */
 function Dropdown(el) {
   if (!(el instanceof HTMLSelectElement)) {
     throw new Error('Dropdown requires a <select> element');
   }
   this.el = el;
   this.el.addEventListener('change', this.onSelect.bind(this));
-  this.eventMap = {};
 }
 
 Dropdown.prototype = {
-  onSelect: function onSelect(evt) {
+  /**
+   * Handler for select's "onchange" event. Fires the event out.
+   */
+  _onSelect: function onSelect(evt) {
     this.fire('change', this.el.value);
+  },
+
+  /**
+   * Returns the value of the dropdown
+   *
+   * @method getValue
+   * @return {String} The value of the dropdown
+   */
+  getValue: function getValue() {
+    return this.el.value;
   }
 };
 
